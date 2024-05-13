d701b4f0243ae63b3dc1647e8fa99926335b9438
lijun695 <lijun695@jd.com>
2024-05-13 18:02:16 +0800

    Open the links in new windows.

--- lib/asciidoctor-comment-links.rb
+++ lib/asciidoctor-comment-links.rb
@@ -9,7 +9,7 @@ module AsciidoctorCommentLinks
       formatter.singleton_class.prepend (Module.new do
         def safe_span tok, safe_val
           if tok.token_chain[0].matches? ::Rouge::Token::Tokens::Comment
-            safe_val = safe_val.gsub %r/https?:\/\/\S+/, '<a href="\&">\&</a>'
+            safe_val = safe_val.gsub %r/https?:\/\/\S+/, '<a href="\&" target="_blank">\&</a>'
           end
           super
         end
